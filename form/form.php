c7ddbd80e3d9a588982550f5b04ee22bd746ea83
Nadya <izam.nadya@mail.ru>
2024-02-23 23:08:54 +0300

    Validation

--- form/form.php
+++ form/form.php
@@ -19,23 +19,29 @@
   <form action="" method="POST">
     <h1>Форма</h1>
 
-    <div class="form-content">
+    <?php
+    if (!empty($messages)) {
+      print('<div id="messages">');
+      // Выводим все сообщения.
+      foreach ($messages as $message) {
+        print($message);
+      }
+      print('</div>');
+    }
+    ?>
 
+    <div class="form-content">
       <div class="form-item">
-        <div name="nameErr"><?php echo $nameErr;?></div>
-        <label class="" for="name">ФИО</label><br>
-          <!-- <input class="line" name="name"> -->
-        <input class="input name" type="text" name="name">
+        <label class="" for="name" <?php if ($errors['name']) {print 'class="error"';} ?>>ФИО</label><br>
+        <input class="input name" type="text" name="name" value="<?php echo $values['name']; ?>">
       </div>
       <div class="form-item">
-        <label class="" for="phone">Teлефон</label><br>
-          <!-- <input class="line" name="phone"> -->
-        <input class="input tel" type="tel" placeholder="" name="phone">
+        <label class="" for="phone" <?php if ($errors['phone1'] || $errors['phone2']) {print 'class="error"';} ?>>Teлефон</label><br>
+        <input class="input tel" type="tel" placeholder="" name="phone" value="<?php print $values['phone']; ?>">
       </div>
       <div class="form-item">
-        <!-- <input class="line" name="email"> -->
-          <label class="" for="email">Email</label> <br>
-        <input class="input email" type="email"  name="email">
+          <label class="" for="email" <?php if ($errors['email1'] || $errors['email2']) {print 'class="error"';} ?>>Email</label> <br>
+        <input class="input email" type="email"  name="email" value="<?php print $values['email']; ?>">
       </div>
       <div class="date">
         <div class="date-item">
@@ -59,12 +65,16 @@
         </select>
         </div>
         <div class="date-item">
-        <span>Год :</span>
+        <span <?php if ($errors['year']) {print 'class="error"';} ?>>Год :</span>
         <select name="year">
-          <?php
-          for ($i = 2024; $i >= 1922; $i--) {
-            printf('<option value="%d">%d </option>', $i, $i);
-          }
+          <?php 
+            for ($i = 2024; $i >= 1922; $i--) {
+              if ($i == $values['year']) {
+                printf('<option selected value="%d">%d год</option>', $i, $i);
+              } else {
+              printf('<option value="%d">%d год</option>', $i, $i);
+              }
+            }
           ?>
         </select>
         </div>
@@ -74,40 +84,40 @@
         <p>Пол:</p>
         <ul>
           <li>
-            <input type="radio" id="radioMale" name="pol" value="male" checked>
+            <input type="radio" id="radioMale" name="pol" value="male"  <?php if ($values['gender'] == 'male') {print 'checked';} ?> checked>
             <label for="radioMale">Муж</label>
           </li>
           <li>
-            <input type="radio" id="radioFemale" name="pol" value="female">
+            <input type="radio" id="radioFemale" name="pol" value="female" <?php if ($values['gender'] == 'female') {print 'checked';} ?>>
             <label for="radioFemale">Жен</label>
           </li>
         </ul>
       </div>
       <div class="form-item">
-        <p>Любимый язык программирования:</p>
+        <p <?php if ($errors['languages']) {print 'class="error"';} ?> >Любимый язык программирования:</p>
         <ul>
           <li>
-            <input type="checkbox" id="JS" name="languages[]" value='JS'>
+            <input type="checkbox" id="JS" name="languages[]" value='JS' <?php if (!empty($values['languages'])) {print 'checked';}?>>
             <label for="JS">JS</label>
           </li>
           <li>
-            <input type="checkbox" id="Python" name="languages[]" value='Python'>
+            <input type="checkbox" id="Python" name="languages[]" value='Python' <?php if (!empty($values['languages'])) {print 'checked';}?>>
             <label for="Python">Python</label>
           </li>
           <li>
-            <input type="checkbox" id="C++" name="languages[]" value='C++'>
+            <input type="checkbox" id="C++" name="languages[]" value='C++' <?php if (!empty($values['languages'])) {print 'checked';}?>>
             <label for="C++">C++</label>
           </li>
         </ul>
       </div>
       <div class="form-item">
-        <p class="big-text">О себе:</p>
+        <p class="big-text" <?php if ($errors['biography1'] || $errors['biography2']) {print 'error';} ?>>О себе:</p>
         <textarea name="biography" cols=24 rows=4 maxlength=128></textarea>
       </div>
     </div>
     <div class="send">
       <div class="contract">
-        <input type="checkbox" id="checkboxContract" name="checkboxContract">
+        <input type="checkbox" id="checkboxContract" name="checkboxContract" <?php if ($values['checkboxContract'] == '1') {print 'checked';} ?> >
         <label for="checkboxContract">С контрактом ознакомлен(а)</label>
       </div>
       <input class="btn" type="submit" name="submit" value="Отправить" />
--- form/index.php
+++ form/index.php
@@ -3,126 +3,316 @@ error_reporting(E_ALL ^ E_WARNING);
 header('Content-Type: text/html; charset=UTF-8');
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  if (!empty($_GET['save'])) {
-    print('
-      <p>
-        Спасибо, результаты сохранены.
-      </p>
-      ');
+  $messages = array();
+  if (!empty($_COOKIE['save'])) {
+      setcookie('save', '', 100000);
+      $messages[] = '<div class="good">Спасибо, результаты сохранены</div>';
   }
+  $errors = array();
+  $errors['name'] = !empty($_COOKIE['name_error']);
+  $errors['phone1'] = !empty($_COOKIE['phone_error1']);
+  $errors['phone2'] = !empty($_COOKIE['phone_error2']);
+  $errors['email1'] = !empty($_COOKIE['email_error1']);
+  $errors['email2'] = !empty($_COOKIE['email_error2']);
+  $errors['year'] = !empty($_COOKIE['year_error']);
+  $errors['gender'] = !empty($_COOKIE['gender_error']);
+  $errors['languages'] = !empty($_COOKIE['languages_error']);
+  $errors['biography1'] = !empty($_COOKIE['biography_error1']);
+  $errors['biography2'] = !empty($_COOKIE['biography_error2']);
+  $errors['checkboxContract'] = !empty($_COOKIE['checkboxContract_error']);
+
+  if ($errors['name']) {
+    setcookie('name_error', '', 100000);
+    $messages['name'] = '<p class="msg">Заполните поле ФИО</p>';
+  }
+  if ($errors['email1']) {
+    setcookie('email_error1', '', 100000);
+    $messages['email1'] = '<p class="msg">Заполните поле email</p>';
+  } else if ($errors['email2']) {
+    setcookie('email_error2', '', 100000);
+    $messages['email2'] = '<p class="msg">Неверно заполнено поле email</p>';
+  }
+  if ($errors['phone1']){
+    setcookie('phone_error1','',100000);
+    $messages['phone1']='<p class="msg">Заполните поле телефон</p>';
+  }
+  if ($errors['phone2']){
+    setcookie('phone_errors2','',100000);
+    $messages['phone2']='<p class="msg">Неверно заполнено поле телефон</p>';
+  }
+ if ($errors['year']) {
+    setcookie('year_error', '', 100000);
+    $messages['year'] = '<p class="msg">Вам должно быть 14 лет</p>';
+  }
+  if ($errors['gender']) {
+    setcookie('gender_error', '', 100000);
+    $messages['gender'] = '<p class="msg">Выберите пол</p>';
+  }
+  if ($errors['languages']) {
+    setcookie('languages_error', '', 100000);
+    $messages['languages'] = '<p class="msg">Выберите язык программирования</p>';
+  } 
+  if ($errors['biography1']) {
+    setcookie('biography_error1', '', 100000);
+    $messages['biography1'] = '<p class="msg">Расскажи о себе что-нибудь</p>';
+  } else if ($errors['biography2']) {
+    setcookie('biography_error2', '', 100000);
+    $messages['biography2'] = '<p class="msg">Недопустимый формат ввода (необходимо на русском)</p>';
+  }
+  if ($errors['checkboxContract']) {
+    setcookie('checkboxContract_error', '', 100000);
+    $messages['checkboxContract'] = '<p class="msg">Ознакомьтесь с контрактом</p>';
+  }
+  $values = array();
+  $values['name'] = empty($_COOKIE['name_value']) ? '' : $_COOKIE['name_value'];
+  $values['phone'] = empty($_COOKIE['phone_value']) ? '' : $_COOKIE['phone_value'];
+  $values['email'] = empty($_COOKIE['email_value']) ? '' : $_COOKIE['email_value'];
+  $values['day'] = empty($_COOKIE['day_value']) ? '' : $_COOKIE['day_value'];
+  $values['month'] = empty($_COOKIE['month_value']) ? '' : $_COOKIE['month_value'];
+  $values['year'] = empty($_COOKIE['year_value']) ? '' : $_COOKIE['year_value'];
+  $values['gender'] = empty($_COOKIE['gender_value']) ? '' : $_COOKIE['gender_value'];
+  $values['languages'] = empty($_COOKIE['languages_value']) ? '' : $_COOKIE['languages_value'];
+  $values['biography'] = empty($_COOKIE['biography_value']) ? '' : $_COOKIE['biography_value'];
+  $values['checkboxContract'] = empty($_COOKIE['checkboxContract_value']) ? '' : $_COOKIE['checkboxContract_value'];
   include('form.php');
-  exit();
-}
+}else {
+  $errors = FALSE;
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$day = $_POST['day'];
-$month = $_POST['month'];
-$year = $_POST['year'];
-$pol = $_POST['pol'];
-$phone = $_POST['phone'];
-$biography = $_POST['biography'];
-$checkboxContract=$_POST['checkboxContract'];
-if (isset($_POST["languages"])) {
-  $languages = $_POST["languages"];
-  $filtred_languages = array_filter(
-    $languages,
-    function ($value) {
-      return ($value == 'JS' || $value == 'Python' || $value == 'C++');
-    }
-  );
-}
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $day = $_POST['day'];
+  $month = $_POST['month'];
+  $year = $_POST['year'];
+  $pol = $_POST['pol'];
+  $phone = $_POST['phone'];
+  $biography = $_POST['biography'];
+  $checkboxContract=$_POST['checkboxContract'];
 
-$errors = FALSE;
+    if (isset($_POST["languages"])) {
+    $languages = $_POST["languages"];
+    $filtred_languages = array_filter(
+      $languages,
+      function ($value) {
+        return ($value == 'JS' || $value == 'Python' || $value == 'C++');
+      }
+    );
+  }
 
-if (empty($name)) {
-  print('
-    <h3>
-      Заполните имя.
-    </h3>');
-  $errors = TRUE;
-}
-if (empty($phone)) {
-  print('
-    <h3>
-      Заполните телефон.
-    </h3>');
-  $errors = TRUE;
-}
+  if (empty($name)) {
+    setcookie('name_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } else {
+    setcookie('name_value', $name, time() + 30 * 24 * 60 * 60);
+  }
 
-if (empty($email)) {
-  print('
-    <h3>
-      Заполните email.
-    </h3>');
-  $errors = TRUE;
-}
+  if (empty($phone)) {
+    setcookie('phone_error1', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } else if (!filter_var($phone, FILTER_VALIDATE_EMAIL)) {
+    setcookie('phone_error2', '1', time() + 24 * 60 * 60);
+    setcookie('phone_value', $phone, time() + 30 * 24 * 60 * 60);
+    $errors = TRUE;
+  } else {
+    setcookie('phone_value', $phone, time() + 30 * 24 * 60 * 60);
+  }
 
-if (!is_numeric($year)) {
-  print('
-    <h3>
-      Неправильный формат ввода года.
-    </h3>');
-  $errors = TRUE;
-} else if ((2024 - $year) < 18) {
-  print('
-    <h3>
-      Форма отправляется только совершеннолетними, вам нет 18.
-    </h3>');
-  $errors = TRUE;
-}
+  if (empty($email)) {
+    setcookie('email_error1', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    setcookie('email_error2', '1', time() + 24 * 60 * 60);
+    setcookie('email_value', $email, time() + 30 * 24 * 60 * 60);
+    $errors = TRUE;
+  } else {
+    setcookie('email_value', $email, time() + 30 * 24 * 60 * 60);
+  }
 
-if (empty($languages)) {
-  print('
-    <h3>
-      Выберите любимый язык программирования.
-    </h3>');
-  $errors = TRUE;
-}
+  if ((2024 - $year) < 14) {
+    setcookie('year_error', '1', time() + 24 * 60 * 60);
+    setcookie('year_value', $year, time() + 30 * 24 * 60 * 60);
+    $errors = TRUE;
+  } else {
+    setcookie('year_value', $year, time() + 30 * 24 * 60 * 60);
+  }
+
+  if (empty($gender)) {
+    setcookie('gender_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } else {
+    setcookie('gender_value', $gender, time() + 30 * 24 * 60 * 60);
+  }
+
+  if (empty($languages)) {
+    setcookie('languages_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } else {
+    setcookie('languages_value', serialize($languages), time() + 30 * 24 * 60 * 60);
+  }
+
+  if (empty($biography)) {
+    setcookie('biography_error1', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } else if (!preg_match('/^[\p{Cyrillic}\d\s,.!?-]+$/u', $biography)) {
+    setcookie('biography_error2', '1', time() + 24 * 60 * 60);
+    setcookie('biography_value', $biography, time() + 30 * 24 * 60 * 60);
+    $errors = TRUE;
+  } else {
+    setcookie('biography_value', $biography, time() + 30 * 24 * 60 * 60);
+  }
+  if ($checkboxContract == '') {
+    setcookie('checkboxContract_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } else {
+    setcookie('checkboxContract_value', $checkboxContract, time() + 30 * 24 * 60 * 60);
+  }
+
+  if ($errors) {
+    header('Location: index.php');
+    exit();
+  }
+  else {
+    setcookie('name_error', '', 100000);
+    setcookie('phone_error1', '', 100000);
+    setcookie('phone_error2', '', 100000);
+    setcookie('email_error1', '', 100000);
+    setcookie('email_error2', '', 100000);
+    setcookie('year_error', '', 100000);
+    setcookie('gender_error', '', 100000);
+    setcookie('languages_error', '', 100000);
+    setcookie('biography_error1', '', 100000);
+    setcookie('biography_error2', '', 100000);
+    setcookie('checkboxContract_error', '', 100000);
+  }
 
 
-if (empty($biography)) {
-  print('
-    <h3>
-      Расскажи о себе.
-    </h3>');
-  $errors = TRUE;
-} else if (!preg_match('/^[\p{Cyrillic}\d\s,.!?-]+$/u', $biography)) {
-  print('
-    <h3>
-      Недопустимый формат ввода биографии (русские буквы).
-    </h3>');
-  $errors = TRUE;
-}
+  $user = 'u67359';
+  $pass = '1353557';
+  $db = new PDO('mysql:host=localhost;dbname=u67359', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
 
-if ($checkboxContract == '') {
-  print('
-    <h3>
-      Ознакомьтесь с контрактом.
-    </h3>');
-  $errors = TRUE;
+  try {
+  $stmt = $db->prepare("INSERT INTO application (name, phone, email, day, month, year, pol, biography) VALUES (?, ?,?, ?, ?, ?,?,?)");
+  $stmt->execute([$name, $phone,$email, $day, $month, $year, $pol, $biography]);
+  $application_id = $db->lastInsertId();
+  $stmt = $db->prepare("INSERT INTO languages (application_id, language) VALUES (?, ?)");
+  foreach ($languages as $language) {
+    $stmt->execute([$application_id, $language]);
+  }
+  } catch (PDOException $e) {
+    print('Error : ' . $e->getMessage());
+    exit();
+  }
+  setcookie('save', '1');
+  header('Location: index.php');
 }
+// $name = $_POST['name'];
+// $email = $_POST['email'];
+// $day = $_POST['day'];
+// $month = $_POST['month'];
+// $year = $_POST['year'];
+// $pol = $_POST['pol'];
+// $phone = $_POST['phone'];
+// $biography = $_POST['biography'];
+// $checkboxContract=$_POST['checkboxContract'];
+// if (isset($_POST["languages"])) {
+//   $languages = $_POST["languages"];
+//   $filtred_languages = array_filter(
+//     $languages,
+//     function ($value) {
+//       return ($value == 'JS' || $value == 'Python' || $value == 'C++');
+//     }
+//   );
+// }
 
-if ($errors) {
-  exit();
-}
+// $errors = FALSE;
 
-$user = 'u67359';
-$pass = '1353557';
-$db = new PDO('mysql:host=localhost;dbname=u67359', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+// if (empty($name)) {
+//   print('
+//     <h3>
+//       Заполните имя.
+//     </h3>');
+//   $errors = TRUE;
+// }
+// if (empty($phone)) {
+//   print('
+//     <h3>
+//       Заполните телефон.
+//     </h3>');
+//   $errors = TRUE;
+// }
 
-try {
-$stmt = $db->prepare("INSERT INTO application (name, phone, email, day, month, year, pol, biography) VALUES (?, ?,?, ?, ?, ?,?,?)");
-$stmt->execute([$name, $phone,$email, $day, $month, $year, $pol, $biography]);
-$application_id = $db->lastInsertId();
-$stmt = $db->prepare("INSERT INTO languages (application_id, language) VALUES (?, ?)");
-foreach ($languages as $language) {
-  $stmt->execute([$application_id, $language]);
-}
-} catch (PDOException $e) {
-  print('Error : ' . $e->getMessage());
-  exit();
-}
- header('Location: ?save=1');
+// if (empty($email)) {
+//   print('
+//     <h3>
+//       Заполните email.
+//     </h3>');
+//   $errors = TRUE;
+// }
+
+// if (!is_numeric($year)) {
+//   print('
+//     <h3>
+//       Неправильный формат ввода года.
+//     </h3>');
+//   $errors = TRUE;
+// } else if ((2024 - $year) < 18) {
+//   print('
+//     <h3>
+//       Форма отправляется только совершеннолетними, вам нет 18.
+//     </h3>');
+//   $errors = TRUE;
+// }
+
+// if (empty($languages)) {
+//   print('
+//     <h3>
+//       Выберите любимый язык программирования.
+//     </h3>');
+//   $errors = TRUE;
+// }
+
+
+// if (empty($biography)) {
+//   print('
+//     <h3>
+//       Расскажи о себе.
+//     </h3>');
+//   $errors = TRUE;
+// } else if (!preg_match('/^[\p{Cyrillic}\d\s,.!?-]+$/u', $biography)) {
+//   print('
+//     <h3>
+//       Недопустимый формат ввода биографии (русские буквы).
+//     </h3>');
+//   $errors = TRUE;
+// }
+
+// if ($checkboxContract == '') {
+//   print('
+//     <h3>
+//       Ознакомьтесь с контрактом.
+//     </h3>');
+//   $errors = TRUE;
+// }
+
+// if ($errors) {
+//   exit();
+// }
+
+// $user = 'u67359';
+// $pass = '1353557';
+// $db = new PDO('mysql:host=localhost;dbname=u67359', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+
+// try {
+// $stmt = $db->prepare("INSERT INTO application (name, phone, email, day, month, year, pol, biography) VALUES (?, ?,?, ?, ?, ?,?,?)");
+// $stmt->execute([$name, $phone,$email, $day, $month, $year, $pol, $biography]);
+// $application_id = $db->lastInsertId();
+// $stmt = $db->prepare("INSERT INTO languages (application_id, language) VALUES (?, ?)");
+// foreach ($languages as $language) {
+//   $stmt->execute([$application_id, $language]);
+// }
+// } catch (PDOException $e) {
+//   print('Error : ' . $e->getMessage());
+//   exit();
+// }
+//  header('Location: ?save=1');
 
 ?>
