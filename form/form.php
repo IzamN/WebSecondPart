b23e24e9cee4492e8a1ef124da6f939bc3699c7a
Nadya <izam.nadya@mail.ru>
2024-02-29 17:11:07 +0300

    del

--- form/form.php
+++ form/form.php
@@ -105,19 +105,19 @@
         <ul>
           <li>
             <input type="checkbox" id="JS" name="languages[]" value='1' 
-            <?php if (isset($values['languages']) && !empty($values['languages']) && in_array(1, unserialize($values['languages']))) {print 'checked';}?>
+          
             >
             <label for="JS">JS</label>
           </li>
           <li>
             <input type="checkbox" id="Python" name="languages[]" value='2'
-            <?php if (isset($values['languages']) && !empty($values['languages']) && in_array(2, unserialize($values['languages']))) {print 'checked';}?>
+           
             >
             <label for="Python">Python</label>
           </li>
           <li>
             <input type="checkbox" id="C++" name="languages[]" value='3' 
-            <?php if (isset($values['languages']) && !empty($values['languages']) && in_array(3, unserialize($values['languages']))) {print 'checked';}?>
+            
             >
             <label for="C++">C++</label>
           </li>
