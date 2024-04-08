b163a1c946a855155a4d6e03680e21343350b2ec
IzamN <144022201+IzamN@users.noreply.github.com>
2024-04-08 20:30:52 +0300

    Update authout.php

--- auth/authout.php
+++ auth/authout.php
@@ -1,6 +1,7 @@
 <?php
 $_SERVER['PHP_AUTH_USER']='';
 $_SERVER['PHP_AUTH_PW']='';
-header('Location: ./index.php');
-// include ('auth.php');
+// header('Location: ./index.php');
+header('Location: ../');
+include ('index.php');
 
