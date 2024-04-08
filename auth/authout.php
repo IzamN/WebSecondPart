90fe959fb4efe7527930352d6851feb083d93bdc
IzamN <144022201+IzamN@users.noreply.github.com>
2024-04-08 21:13:35 +0300

    Update authout.php

--- auth/authout.php
+++ auth/authout.php
@@ -4,10 +4,9 @@
 // session_start();
  //session_destroy();
 // header('Location: ./index.php');
-
-
+header('Location: http://u67359.kubsu-dev.ru/web2/auth/');
 include('index.php');
-// header('Location: http://u67359.kubsu-dev.ru/web2/auth/index.php');
+
 exit();
 
 
