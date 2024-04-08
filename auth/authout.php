2642c7e40ad26f9b3821fbe62210f308bcac5c1c
IzamN <144022201+IzamN@users.noreply.github.com>
2024-04-08 21:27:04 +0300

    Update authout.php

--- auth/authout.php
+++ auth/authout.php
@@ -5,7 +5,7 @@
  $_SERVER['PHP_AUTH_PW']='';
 
   // header('Location: ./');
-header('Location: http://u67359.kubsu-dev.ru/web2/auth/');
+// header('Location: http://u67359.kubsu-dev.ru/web2/auth/');
 include('index.php');
 
 exit();
