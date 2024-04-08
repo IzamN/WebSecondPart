35fe4c2318d7bd17d2939130e2b595bd316440bc
IzamN <144022201+IzamN@users.noreply.github.com>
2024-04-08 20:58:19 +0300

    Update authout.php

--- auth/authout.php
+++ auth/authout.php
@@ -5,7 +5,7 @@ session_start();
 session_destroy();
 // header('Location: ./index.php');
 
-header('Location: ././index.php');
+// header('Location: ././index.php');
 include('index.php');
 exit();
 
